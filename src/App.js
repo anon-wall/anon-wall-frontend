88a92640e902545639645e45d10941243d25d8e4
sojungLIMM <thwid9897@gmail.com>
2022-02-06 00:43:09 +0900

    feat: make MainHeader and App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,34 @@
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+import styled from "styled-components";
+
+import MainHeader from "./components/MainHeader";
+
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div>
+      <MainHeader />
+      <Main>
+        <Routes>
+          <Route path="/" />
+          <Route path="/counsels/*">
+            <Route path=":counsel_id/*" />
+            <Route path=":counsel_id/counselors/:user_id" />
+            <Route path=":counsel_id/room" />
+          </Route>
+          <Route path="/counsels/new" />
+          <Route path="/mypage">
+            <Route path="/mypage/counselor" />
+            <Route path="/mypage/stories" />
+          </Route>
+        </Routes>
+      </Main>
+    </div>
+  );
 }
 
 export default App;
+
+const Main = styled.main`
+  margin: 20px;
+`;
