f0870655c638a2e4a460bf150ec0303ced95357f
Sangmin Na <sangmin.iam@gmail.com>
2022-02-05 22:19:47 +0900

    feat: modal

--- src/index.js
+++ src/index.js
@@ -1,12 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { BrowserRouter } from "react-router-dom";
 
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
+import GlobalStyle from "./theme/globalStyle";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <GlobalStyle />
+      <App />
+    </BrowserRouter>
   </React.StrictMode>,
   document.getElementById("root")
 );
