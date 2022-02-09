78ae2d94247f412292d724b4ac92280d7cce6dfd
Sangmin Na <sangmin.iam@gmail.com>
2022-02-09 23:21:29 +0900

    feat: CounselorDetail, ReservationDate

--- src/App.js
+++ src/App.js
@@ -6,6 +6,8 @@ import Home from "./pages/Home";
 import NotFound from "./components/NotFound";
 import StoryList from "./pages/StoryList";
 import StoryDetail from "./pages/StoryDetail";
+import StoryDetail from "./components/StroyDetail";
+import CounselorDetail from "./components/CounselorDetail";
 
 function App() {
   return (
@@ -16,7 +18,10 @@ function App() {
           <Route path="/" element={<Home />} />
           <Route path="/counsels" element={<StoryList />} />
           <Route path="/counsels/:counsel_id" element={<StoryDetail />} />
-          <Route path="/counsels/:counsel_id/counselors/:user_id" />
+          <Route
+            path="/counsels/:counsel_id/counselors/:user_id"
+            element={<CounselorDetail />}
+          />
           <Route path="/counsels/:counsel_id/room" />
           <Route path="/counsels/new" />
           <Route path="/mypage">
