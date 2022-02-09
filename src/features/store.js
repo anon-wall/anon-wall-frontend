de5101fd6c4e73d1c60dab5b471141730c8d7700
sojungLIMM <thwid9897@gmail.com>
2022-02-09 09:40:40 +0900

    chore: delete paginationSlicer and use state

--- src/features/store.js
+++ src/features/store.js
@@ -2,16 +2,15 @@ import { configureStore } from "@reduxjs/toolkit";
 import logger from "redux-logger";
 
 import userReducer from "./userSlice";
-import paginationReducer from "./paginationSlice";
 
 const store = configureStore({
-  reducer: { user: userReducer, pagination: paginationReducer },
+  reducer: { user: userReducer },
   middleware: (getDefaultMiddleware) => {
     if (process.env.NODE_ENV !== "production") {
       getDefaultMiddleware({ serializableCheck: false }).concat(logger);
     }
 
-    return getDefaultMiddleware({ serializableCheck: false }).concat(logger);
+    return getDefaultMiddleware();
   },
 });
 
