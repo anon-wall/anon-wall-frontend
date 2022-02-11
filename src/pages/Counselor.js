39dca94c2e31b9e68b6572590e6ab04ac52d45c9
sojungLIMM <thwid9897@gmail.com>
2022-02-11 12:27:31 +0900

    style: SideBar

---
+++ src/pages/Counselor.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+import MyPageWrapper from "../components/shared/MyPageWrapper";
+
+function Counselor() {
+  return <MyPageWrapper></MyPageWrapper>;
+}
+
+export default Counselor;
