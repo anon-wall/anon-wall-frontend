5f1ff56158caa3fe7dfce6a8458ac610f92a6db4
Sangmin Na <sangmin.iam@gmail.com>
2022-02-17 19:41:17 +0900

    refactor: Home, MyPage, StoryList

--- src/components/shared/ImageWrapper.js
+++ src/components/shared/ImageWrapper.js
@@ -4,15 +4,16 @@ const ImageWrapper = styled.div`
   display: flex;
   justify-content: center;
   align-items: center;
-  width: 25rem;
-  height: 25rem;
+  min-width: 25rem;
+  min-height: 25rem;
   background-color: transparent;
 
   img {
-    width: 20rem;
-    height: 20rem;
+    object-fit: cover;
+    min-width: 20rem;
+    min-height: 20rem;
     border-radius: 50%;
-    background-color: #ffffff;
+    background-color: transparent;
   }
 `;
 
--- src/constants/story.js
+++ src/constants/story.js
@@ -2,5 +2,6 @@ export const STORY_SUB_HEADER_HEADING = "고민 담벼락";
 export const STORY_SUB_HEADER_PARAGRAPH =
   "나누고 싶은 고민을 자유롭게 올려주세요. 여러분의 고민을 들어줄 익명의 누군가가 곧 찾아올거에요.";
 export const STORY_ACCEPT_SUCCESS_MESSAGE = "사연 수락이 완료되었습니다.";
+export const STORY_RESULT_MESSAGE = "검색 결과가 없습니다.";
 export const PREV = "< prev";
 export const NEXT = "next >";
